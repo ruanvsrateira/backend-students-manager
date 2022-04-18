d2182d4154284dc362f9aacde9d4d29e744e331b
ruanvsrateira <101360876+ruanvsrateira@users.noreply.github.com>
2022-04-18 18:25:01 -0300

    feat: add tests, install pg for destroy db a tests

--- prisma/prisma-environment-jest.js
+++ prisma/prisma-environment-jest.js
@@ -2,6 +2,7 @@ const  NodeEnvironment = require('jest-environment-node')
 const {v4: uuid} = require('uuid');
 const { execSync } = require('child_process');
 const { resolve } = require('path'); 
+const { Client } = require('pg');
 
 const prismaCli = resolve(__dirname, '..', 'node_modules', '.bin', 'prisma'); 
 
@@ -24,7 +25,17 @@ class CustomEnvironment extends NodeEnvironment {
 
         //rodar as migrations
         execSync(`${prismaCli} migrate dev`)
-    }
+    };
+
+    async teardown() {
+        const client = new Client({
+          connectionString: this.connectionString,
+        });
+    
+        await client.connect();
+        await client.query(`DROP SCHEMA IF EXISTS "${this.schema}" CASCADE`);
+        await client.end();
+      }
 }
 
 module.exports =  CustomEnvironment;
--- test/test.spec.ts
+++ test/test.spec.ts
@@ -5,15 +5,95 @@
 import app from '../src/app';
 import request from "supertest";
 
- describe("Create Student Controller", () => {
-   it("Should be able to create a new user", async () => {
-     const response = await request(app).post("/students").send({
-       name: "ruan",
-       email: "ruan@ruan.com",
-       age: 18,
-       cpf: "123-123-123-123"
-     })
+describe("Create Student Controller", () => {
+  it("Should be able to create a new student", async () => {
+    const response = await request(app).post("/students").send({
+      name: "teste1",
+      email: "teste1@teste.com",
+      age: 18,
+      cpf: "123-123-123-123"
+    })
 
-     console.log(response.body)
-   });
- });
+    expect(response.status).toEqual(200);
+  });
+
+  it("Should be not able create a new student", async() => {
+    await request(app).post('/students').send({
+      name: "teste2",
+      email: "teste2@teste.com",
+      age: 19,
+      cpf: "22-22-22-22"
+    });
+    
+    const response = await request(app).post('/students').send({
+      name: "teste2",
+      email: "teste2@teste.com",
+      age: 19,
+      cpf: "22-22-22-22"
+    });
+
+    expect(response.body.error).toEqual("there is already a student with this email")
+    
+  });
+});
+
+describe("Get all Students", () => {
+  it("Should be able returns students", async() => {
+    const response = await request(app).get("/students");
+
+    expect(typeof response.body.students).toBe("object");
+  });
+});
+
+describe("Delete Student a id equal a 1", () => {
+  it("Should be able Delete Dtudent Service", async() => {
+    const response = await request(app).get("/students/1/delete");
+
+    expect(typeof response.body.student_deleted).toBe("object");
+  });
+
+  it("Should be show error, this student by this id not exist", async() => {
+    const response = await request(app).get("/students/1/delete");
+
+    expect(response.body.error).toEqual("no student with this id was found");
+    expect(typeof response.body).toBe("object")
+  });
+});
+
+describe("Edit Student Service", () => {
+  it("Should able edit a user a id equal a 2", async() => {
+    const response = await request (app).post("/students/2/edit").send({
+      name: "test3",
+      email: "test3@test.com",
+      age: 19,
+      cpf: "22-22-22-22",
+    });
+
+    expect(response.status).toEqual(200);
+    expect(typeof response.body).toBe("object")
+  });
+
+  it("Should not able edit user by id equal a 1 not exist", async() => {
+    const response = await request(app).post("/students/1/edit").send({
+      name: " ",
+      email: " ",
+      age: 0,
+      cpf: " ",
+    });
+
+    expect(typeof response.body).toBe("object");
+    expect(response.body.error).toEqual("this id not exist");
+  })
+
+  it("Should not able edit use by email equal a another user", async() => {
+    const response = await request(app).post("/students/2/edit").send({
+      name: "test3",
+      email: "test3@test.com",
+      age: 19,
+      cpf: "22-22-22-22"
+    });
+
+    expect(typeof response.body).toBe("object");
+    expect(response.body.error).toEqual("email already used by another student");
+  });
+});
